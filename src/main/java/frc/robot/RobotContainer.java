81d7e7c1745189915a648c260c770af6cb6d2b5f
poopface-exploded <aproblem924@gmail.com>
2023-11-05 14:14:05 -0800

    added "stopMotorsAndRunOnce", defined required subsystem instance (ArmSubsystem)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,18 @@
 
 package frc.robot;
 
+import javax.management.RuntimeOperationsException;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.TestArmCommand;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,6 +26,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
 	// The robot's subsystems and commands are defined here...
 	private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+	private final ArmSubsystem armSubsystem = new ArmSubsystem(); 
 
 	// Replace with CommandPS4Controller or CommandJoystick if needed
 	private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.DRIVER_CONTROLLER_PORT);
@@ -48,6 +54,8 @@ public class RobotContainer {
 		// Schedule `exampleMethodCommand` when the Xbox controller's B button is
 		// pressed, cancelling on release.
 		m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+		
+		Command stopMotorsAndRunOnce = Commands.runOnce(() -> armSubsystem.stopAllMotors()).andThen(() -> new TestArmCommand(armSubsystem));
 	}
 
 	/**
